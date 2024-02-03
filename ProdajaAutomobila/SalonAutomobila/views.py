435b8a0882f6d9b62da8de6c71940a5872c3f642
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2024-02-03 19:43:09 +0100

    Osposobljena lista i prikaz artikala

--- ProdajaAutomobila/SalonAutomobila/views.py
+++ ProdajaAutomobila/SalonAutomobila/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Segment, Automobil
 
 # Create your views here.
+def ListaAutomobila(request, segment_slug=None):
+    segment = None
+    segmenti = Segment.objects.all()
+    automobili = Automobil.objects.filter(raspoloziv=True)
+    if segment_slug:
+        segment = get_object_or_404(Segment, slug=segment_slug)
+        automobili = automobili.filter(segment=segment)
+    return render(request, 'SalonAutomobila/automobil/list.html', {'segment': segment, 'segmenti': segmenti, 'automobili': automobili})
+
+def DetaljiAutomobila(request, id, slug):
+    automobil = get_object_or_404(Automobil, id=id, slug=slug, raspoloziv=True)
+    return render(request, 'SalonAutomobila/automobil/detail.html', {'automobil':automobil})
