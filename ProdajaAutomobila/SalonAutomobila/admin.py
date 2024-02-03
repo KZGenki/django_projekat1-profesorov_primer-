c2d06c1f99ece8d7523e9db9b2ceff67eb13c006
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2024-02-03 14:06:20 +0100

    napravljeni admin modeli za segment i automobil modele python manage.py createsuperuser

--- ProdajaAutomobila/SalonAutomobila/admin.py
+++ ProdajaAutomobila/SalonAutomobila/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Segment, Automobil
 
 # Register your models here.
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ['naziv', 'slug']
+    prepopulated_fields = {'slug': ('naziv',)}
+
+@admin.register(Automobil)
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ['naziv', 'slug', 'cena', 'raspoloziv', 'kreiran', 'azuriran']
+    list_filter = ['raspoloziv', 'kreiran', 'azuriran']
+    list_editable = ['cena', 'raspoloziv']
+    prepopulated_fields = {'slug': ('naziv',)}
